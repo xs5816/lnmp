bac6f77afa4a18963ac5252ebb2abac76a2c2dd5
xs5816 <xs5816@hotmail.com>
2017-10-21 02:32:56 +0800

    增加redis的安装脚本

--- install/include/redis.sh
+++ install/include/redis.sh
@@ -1,4 +1,25 @@
 Install_redis()
 {
-    echo 'Redis version: '
+    cd ${cur_dir}/redis
+    if [ ${enable_redis} == 'n' ]; then
+        return
+    fi
+    if [ -d "/usr/local/redis-3.2.6" ]; then
+        return
+    fi
+
+    if [ ! -s {$cur_dir}/redis/redis-3.2.6.tar.gz ]; then
+        wget -c 'http://download.redis.io/releases/redis-3.2.6.tar.gz'
+    fi
+    tar zxf redis-3.2.6.tar.gz
+    cd redis-3.2.6
+    make
+    cd src && make install PREFIX=/usr/local/redis-3.2.6
+    cd ../
+    mkdir -p /usr/local/redis-3.2.6/etc/
+    cp redis.conf /usr/local/redis-3.2.6/etc/redis.conf
+    sed -i 's#daemonize\sno#daemonize yes#g' /usr/local/redis-3.2.6/etc/redis.conf
+    /usr/local/redis-3.2.6/bin/redis-server /usr/local/redis-3.2.6/etc/redis.conf
+    cd ../
+    rm -rf redis-3.2.6
 }
