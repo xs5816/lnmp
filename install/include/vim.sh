436aea480d827aaa46ec128124fce8c4b33caf5c
xs5816 <xs5816@hotmail.com>
2017-10-22 10:38:53 +0800

    修正语法错误，缺少半边括号 对一些行进行缩进

--- install/include/vim.sh
+++ install/include/vim.sh
@@ -43,34 +43,34 @@ set background=dark
 colorscheme solarized
 " colorscheme molokai
 EOF
-mkdir -p /root/.vim/bundle
-bundle_dir=/root/.vim/bundle
-cd ${bundle_dir}
-if [ ! -d ${bundle_dir}/vundle ]; then
-    git clone 'https://github.com/gmarik/vundle.git'
-fi
-cd ${bundle_dir}
-if [ ! -d ${bundle_dir}/nerdtree ]; then
-    git clone 'https://github.com/scrooloose/nerdtree.git'
-fi
-cd ${bundle_dir}
-if [ ! -d ${bundle_dir}/tagbar ]; then
-    git clone 'https://github.com/majutsushi/tagbar.git'
-fi
-cd ${bundle_dir}
-if [ ! -d ${bundle_dir}/molokai ]; then
-    git clone 'https://github.com/tomasr/molokai.git'
-fi
-cd ${bundle_dir}
-if [ ! -d ${bundle_dir}/vim-colors-solarized ]; then
-    git clone 'https://github.com/brantb/vim-colors-solarized.git'
-fi
-cd ${bundle_dir}
-if [ ! -d ${bundle_dir}/ctrlp.vim ]; then
-    git clone 'https://github.com/kien/ctrlp.vim.git'
-fi
-cd ${bundle_dir}
-if [ ! -d ${bundle_dir}/vim-powerline ]; then
-    git clone 'https://github.com/lokaltog/vim-powerline.git'
-fi
-
+    mkdir -p /root/.vim/bundle
+    bundle_dir=/root/.vim/bundle
+    cd ${bundle_dir}
+    if [ ! -d ${bundle_dir}/vundle ]; then
+        git clone 'https://github.com/gmarik/vundle.git'
+    fi
+    cd ${bundle_dir}
+    if [ ! -d ${bundle_dir}/nerdtree ]; then
+        git clone 'https://github.com/scrooloose/nerdtree.git'
+    fi
+    cd ${bundle_dir}
+    if [ ! -d ${bundle_dir}/tagbar ]; then
+        git clone 'https://github.com/majutsushi/tagbar.git'
+    fi
+    cd ${bundle_dir}
+    if [ ! -d ${bundle_dir}/molokai ]; then
+        git clone 'https://github.com/tomasr/molokai.git'
+    fi
+    cd ${bundle_dir}
+    if [ ! -d ${bundle_dir}/vim-colors-solarized ]; then
+        git clone 'https://github.com/brantb/vim-colors-solarized.git'
+    fi
+    cd ${bundle_dir}
+    if [ ! -d ${bundle_dir}/ctrlp.vim ]; then
+        git clone 'https://github.com/kien/ctrlp.vim.git'
+    fi
+    cd ${bundle_dir}
+    if [ ! -d ${bundle_dir}/vim-powerline ]; then
+        git clone 'https://github.com/lokaltog/vim-powerline.git'
+    fi
+}
